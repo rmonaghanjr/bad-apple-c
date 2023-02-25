973896078055e2ed9d35e677b33fd9d1dc225418
aejester <aftereffectsjester@gmail.com>
2023-02-25 15:34:23 -0500

    update makefile, add newlines to end of all files

--- makefile
+++ makefile
@@ -1,24 +1,46 @@
-CFILES=src/main.c src/bmp.c src/process.c src/render.c src/util.c
-COMPILER=gcc
-CDEBUG=-g
-CFLAGS=-Wall
-OUT=cmd-play
-INPUT=input.mp4 8
+# project config
+BUILD_DIR := build
+EXECUTABLE := cmd-play
+FRAMES_FOLDER := ./frames/
+CORES := $(shell sysctl -n hw.logicalcpu)
 
-all:
-	rm -rf build 
-	mkdir build
-	$(COMPILER) $(CFLAGS) $(CFILES) -o ./build/$(OUT)
+# compiler config
+COMPILER := clang
+FLAGS := -c -g -Wall -Werror -pedantic-errors
 
-debug:
-	$(COMPILER) $(CFLAGS) $(CDEBUG) $(CFILES) -o ./build/$(OUT)
+# source files
+RENDER_FILES := process.c render.c 
+VIDEO_FILES := bmp.c
+FILES := main.c util.c $(RENDER_FILES) $(VIDEO_FILES)
+OBJECTS := $(addprefix $(BUILD_DIR)/,$(patsubst %.c,%.o,$(FILES)))
+
+.PHONY: $(EXECUTABLE)
+$(EXECUTABLE): $(BUILD_DIR)/$(EXECUTABLE)
+
+.PRECIOUS: $(BUILD_DIR)/. $(BUILD_DIR)%/.
+
+$(BUILD_DIR)/.:
+	mkdir -p $@
+
+$(BUILD_DIR)%/.:
+	mkdir -p $@
+
+.SECONDEXPANSION:
+
+$(BUILD_DIR)/%.o: src/%.c | $$(@D)/.
+	$(COMPILER) $(FLAGS) $< -o $@
+
+$(BUILD_DIR)/$(EXECUTABLE): $(OBJECTS)
+	$(COMPILER) $^ -o ./$(BUILD_DIR)/$(EXECUTABLE)
 
 run:
 	make
-	./build/$(OUT) ./input/$(INPUT)
+	./$(BUILD_DIR)/$(EXECUTABLE) ./input/input.mp4 --verbose --scale=1 --cores=$(CORES) --frames-folder=$(FRAMES_FOLDER)
+
+new:
+	make clean
+	make
 
 clean:
-	rm -rf ./frames
-	rm -rf build 
+	rm -rf build
 	mkdir build
-	clear
--- src/bmp.c
+++ src/bmp.c
@@ -17,53 +17,53 @@ int read_frame(FRAME* result, char* filename) {
     fseek(fp, 0, SEEK_SET);
 
     for (int i = 0; i < 14; i++) {
-        (*result).frame_file_header[i] = fgetc(fp);
+        result->frame_file_header[i] = fgetc(fp);
     }
 
-    if ((*result).frame_file_header[0] != 'B' || (*result).frame_file_header[1] != 'M') {
+    if (result->frame_file_header[0] != 'B' || result->frame_file_header[1] != 'M') {
         printf("invalid header file!\n");
         return 0;
     }
 
-    unsigned int *array_offset_ptr = (unsigned int *)((*result).frame_file_header + 10);
-    (*result).pixel_array_offset = *array_offset_ptr;
+    unsigned int *array_offset_ptr = (unsigned int *)(result->frame_file_header + 10);
+    result->pixel_array_offset = *array_offset_ptr;
 
-    if ((*result).frame_file_header[11] != 0 || (*result).frame_file_header[12] != 0 || (*result).frame_file_header[13] != 0) {
+    if (result->frame_file_header[11] != 0 || result->frame_file_header[12] != 0 || result->frame_file_header[13] != 0) {
         printf("unknown error!\n");
         return 1;
     }
 
     for (int i = 0; i < 40; i++) {
-        fseek(fp, (*result).pixel_array_offset, SEEK_CUR + sizeof(int));
-        (*result).frame_info_header[i] = (int) fgetc(fp);
+        fseek(fp, result->pixel_array_offset, SEEK_CUR + sizeof(int));
+        result->frame_info_header[i] = (int) fgetc(fp);
     }
 
-    int *width_ptr = (int *)((*result).frame_info_header + 4);
-    int *height_ptr = (int *)((*result).frame_info_header + 8);
+    int *width_ptr = (int *)(result->frame_info_header + 4);
+    int *height_ptr = (int *)(result->frame_info_header + 8);
 
-    (*result).width = *width_ptr;
-    (*result).height = *height_ptr;
+    result->width = *width_ptr;
+    result->height = *height_ptr;
 
-    (*result).bits_per_pixel = (*result).frame_info_header[14];
-    if ((*result).bits_per_pixel != 24) {
+    result->bits_per_pixel = result->frame_info_header[14];
+    if (result->bits_per_pixel != 24) {
         printf("invalid file!\n");
     }
 
-    int compressionMethod = (*result).frame_info_header[16];
+    int compressionMethod = result->frame_info_header[16];
     if (compressionMethod != 0) {
         printf("unhandled compression type!\n");
         return 0;
     }
 
-    (*result).row_size = (int) (floor(((*result).bits_per_pixel * (*result).width + 31.) / 32)) * 4;
-    (*result).pixel_array_size = (*result).row_size * abs((*result).height);
+    result->row_size = (int) (floor((result->bits_per_pixel * result->width + 31.) / 32)) * 4;
+    result->pixel_array_size = result->row_size * abs(result->height);
 
-    (*result).pixel_data = (unsigned char*) malloc(sizeof(char) * (*result).pixel_array_size);
+    result->pixel_data = (unsigned char*) malloc(sizeof(char) * result->pixel_array_size);
 
-    fseek(fp, (*result).pixel_array_offset, SEEK_SET);
-    for (int i = 0; i < (*result).pixel_array_size; i++) {
-        fseek(fp, (*result).pixel_array_offset, SEEK_CUR + sizeof(uint32_t));
-        (*result).pixel_data[i] = (uint32_t) fgetc(fp);
+    fseek(fp, result->pixel_array_offset, SEEK_SET);
+    for (int i = 0; i < result->pixel_array_size; i++) {
+        fseek(fp, result->pixel_array_offset, SEEK_CUR + sizeof(uint32_t));
+        result->pixel_data[i] = (uint32_t) fgetc(fp);
     }
 
     fclose(fp);
@@ -72,12 +72,12 @@ int read_frame(FRAME* result, char* filename) {
 }
 
 int pixel_at(FRAME *frame, unsigned int* r, unsigned int* g, unsigned int* b, int x, int y) {
-    if (x < (*frame).width && y < (*frame).height) {
-        y = (*frame).height - 1 - y;
+    if (x < frame->width && y < frame->height) {
+        y = frame->height - 1 - y;
         
-        (*r) = (unsigned int)((*frame).pixel_data[(*frame).row_size * y + 3 * x + 2]);
-        (*g) = (unsigned int)((*frame).pixel_data[(*frame).row_size * y + 3 * x + 1]);
-        (*b) = (unsigned int)((*frame).pixel_data[(*frame).row_size * y + 3 * x + 0]);
+        (*r) = (unsigned int)(frame->pixel_data[frame->row_size * y + 3 * x + 2]);
+        (*g) = (unsigned int)(frame->pixel_data[frame->row_size * y + 3 * x + 1]);
+        (*b) = (unsigned int)(frame->pixel_data[frame->row_size * y + 3 * x + 0]);
         return 1;
     } else {
         return 0;
